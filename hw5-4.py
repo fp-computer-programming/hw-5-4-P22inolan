be6541ae7ac36d0d17513d80bbf48be6d4a100f5
P22inolan <p22inolan@fairfieldprep.org>
2021-10-29 11:11:16 -0400

    Submit 5-4

---
+++ hw5-4.py
@@ -0,0 +1,22 @@
+# Author: IBN (ADMG) 10/29/2021
+
+# Question 1
+print("Python"[2])
+# Question 2
+print("Strings are simply a sequence of characters."[30:32])
+# Question 3
+print(len("excellent"))
+# Question 4
+print("apple" in "pineapple")
+# Question 5
+print("Peach" > "pear")
+# Question 6
+print("x" * 100)
+# Question 7
+print(str(54321))
+# Question 8
+print("p" not in "Pineapple")
+# Question 9
+print('"Livin\' On a Prayer" is my friend\'s favorite song.')
+# Question 10
+print(len('"Livin\' On a Prayer" is my friend\'s favorite song.'))
